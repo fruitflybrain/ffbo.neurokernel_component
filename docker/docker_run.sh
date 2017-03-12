d9343dbc5ad90eb7154882809bf10cd3659fefdc
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2017-03-12 18:23:34 -0400

    remove neuroarch volume mount from docker_run

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,2 +1,2 @@
 docker rm neurokernel_component
-nvidia-docker run --name neurokernel_component -v $(dirname `pwd`):/neurokernel_component  -v $(dirname $(dirname `pwd`))/ffbo.neuroarch:/neuroarch -it ffbo/neurokernel_component:develop sh /neurokernel_component/neurokernel_component/run_component_docker.sh
+nvidia-docker run --name neurokernel_component -v $(dirname `pwd`):/neurokernel_component -it ffbo/neurokernel_component:develop sh /neurokernel_component/neurokernel_component/run_component_docker.sh
