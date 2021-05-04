75817431835bfe8210ee83b39a668e6039c6ce1b
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-05-04 13:41:53 -0400

    workaround for setuptools issue

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
             'neurokernel',
             'neurodriver',
             'neuroarch',
-            'msgpack',
+            'msgpack > 1.0',
             'msgpack-numpy',
             'pyopenssl',
             'txaio',
