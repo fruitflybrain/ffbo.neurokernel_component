2540fa8fe1f063b69b27afb09855056f2dee048e
Yiyin Zhou <yiyin.zhou@fordham.edu>
2024-07-04 14:01:58 -0400

    comment out intermediate certificate file

--- neurokernel_component/neurokernel_component.py
+++ neurokernel_component/neurokernel_component.py
@@ -94,7 +94,7 @@ realm = config["SERVER"]["realm"]
 authentication = eval(config["AUTH"]["authentication"])
 debug = eval(config["DEBUG"]["debug"])
 ca_cert_file = config["AUTH"]["ca_cert_file"]
-intermediate_cert_file = config["AUTH"]["intermediate_cert_file"]
+#intermediate_cert_file = config["AUTH"]["intermediate_cert_file"]
 
 #logging.basicConfig()
 #logging.getLogger("twisted").setLevel(logging.CRITICAL)
@@ -766,9 +766,9 @@ if __name__ == '__main__':
     parser.add_argument('--ca_cert', dest='ca_cert_file', type=six.text_type,
                         default=ca_cert_file,
                         help='Root CA PEM certificate file (defaults to value from config.ini).')
-    parser.add_argument('--int_cert', dest='intermediate_cert_file', type=six.text_type,
-                        default=intermediate_cert_file,
-                        help='Intermediate PEM certificate file (defaults to value from config.ini).')
+    # parser.add_argument('--int_cert', dest='intermediate_cert_file', type=six.text_type,
+    #                     default=intermediate_cert_file,
+    #                     help='Intermediate PEM certificate file (defaults to value from config.ini).')
     parser.add_argument('--no-ssl', dest='ssl', action='store_false')
     parser.set_defaults(ssl=ssl)
     parser.set_defaults(debug=debug)
@@ -789,10 +789,10 @@ if __name__ == '__main__':
         c=OpenSSL.crypto
         ca_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
 
-        st_cert=open(args.intermediate_cert_file, 'rt').read()
-        intermediate_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
+        # st_cert=open(args.intermediate_cert_file, 'rt').read()
+        # intermediate_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
 
-        certs = OpenSSLCertificateAuthorities([ca_cert, intermediate_cert])
+        certs = OpenSSLCertificateAuthorities([ca_cert]) #, intermediate_cert])
         ssl_con = CertificateOptions(trustRoot=certs)
 
         # now actually run a WAMP client using our session class ClientSession
